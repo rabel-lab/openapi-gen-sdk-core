20c313836f26ac3a7cfadef8cc2c913fea9b9ef1
Rabel <46533182+Montreal-Dev@users.noreply.github.com>
2025-12-23 12:55:27 -0500

    Create a Zod utils to enforce validity & transform if necessary;

--- packages/@specnova/src/types/semver.ts
+++ packages/@specnova/src/types/semver.ts
@@ -1,9 +1,25 @@
+import { zodWithTransformativeCheck } from '@/types/utils';
+
 import semverTool from 'semver';
 import z from 'zod';
 
-export const semver = z.string().check(
-  z.trim(),
-  z.refine((val) => semverTool.valid(semverTool.coerce(val)), 'Invalid semver version'),
+export const semver = zodWithTransformativeCheck(
+  z.string(),
+  [
+    (val) => {
+      return { success: true, result: val.trim() };
+    },
+    (val) => {
+      const coerced = semverTool.coerce(val) as semverTool.SemVer;
+      const pass = semverTool.valid(coerced);
+      if (pass && coerced) {
+        return { success: true, result: coerced.format() };
+      } else {
+        return { success: false, error: { message: 'Invalid semver version' } };
+      }
+    },
+  ],
+  (val) => Boolean(semverTool.valid(val)),
 );
 
 export type Semver = z.infer<typeof semver>;
---
+++ packages/@specnova/src/types/utils.ts
@@ -0,0 +1,56 @@
+import z, { ZodType } from 'zod';
+
+type TransformFunc<T extends ZodType> = Parameters<T['transform']>[0];
+type TransformFuncArgs<T extends ZodType> = Parameters<TransformFunc<T>>;
+
+type TransformPass<T extends ZodType> = {
+  success: true;
+  result: z.output<T>;
+};
+
+type TransformFail<T extends ZodType> = {
+  success: false;
+  error: {
+    message: string;
+  };
+};
+
+type TransformCheckFunc<T extends ZodType> = (
+  value: TransformFuncArgs<T>[0],
+  ctx?: TransformFuncArgs<T>[1],
+) => TransformPass<T> | TransformFail<T>;
+
+type TransformValidatorFunc<T extends ZodType> = (value: TransformFuncArgs<T>[0]) => boolean;
+
+export const zodWithTransformativeCheck = <T extends ZodType>(
+  schema: T,
+  transformers: TransformCheckFunc<T>[],
+  validator?: TransformValidatorFunc<T>,
+) => {
+  let vanillaSchema = schema;
+  let transformedValue: T['_output'];
+  //# Transform
+  return vanillaSchema.transform((value, ctx) => {
+    transformedValue = value;
+    //# If validator is present, try to validate
+    if (validator) {
+      if (validator(transformedValue)) {
+        console.log('validator passed');
+        return transformedValue;
+      }
+    }
+    //# Else apply all transformers
+    transformers.forEach((check) => {
+      const checkResult = check(transformedValue, ctx);
+      if (!checkResult.success) {
+        ctx.addIssue({
+          code: 'custom',
+          message: checkResult.error.message,
+        });
+      } else {
+        transformedValue = checkResult.result;
+      }
+    });
+    return transformedValue;
+  });
+};
